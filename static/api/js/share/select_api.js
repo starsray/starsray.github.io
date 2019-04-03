2a53ebef206ca1cb3315ec56d1b0ac8b7ef20593
hconly <3344758676@qq.com>
2019-04-03 10:07:30 +0800

    Site updated: 2019-04-03 10:07:29

--- static/api/js/share/select_api.js
+++ static/api/js/share/select_api.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 03 2019 09:50:10 GMT+0800 (GMT+08:00)
+// build time:Wed Apr 03 2019 10:07:27 GMT+0800 (GMT+08:00)
 window._bd_share_main.F.module("share/select_api",function(e,a,n){var t=e("base/tangram").T,i=e("base/class").Class,s=e("component/comm_tools"),o=e("share/api_base");a.Api=i.create(function(e){var a=this;a._init=function(){var e=a.getView();e.render(),e.init()},a._processAction=function(e){return{data:{type:"select"}}},a._distory=function(){}},o.ApiBase)});
 //rebuild by neat 
