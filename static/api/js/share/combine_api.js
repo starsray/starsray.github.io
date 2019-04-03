354e99a113719d4a4d9241bb322b6928272e7ded
hconly <3344758676@qq.com>
2019-04-03 11:22:45 +0800

    Site updated: 2019-04-03 11:22:44

--- static/api/js/share/combine_api.js
+++ static/api/js/share/combine_api.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 03 2019 10:41:20 GMT+0800 (GMT+08:00)
+// build time:Wed Apr 03 2019 11:22:40 GMT+0800 (GMT+08:00)
 window._bd_share_main.F.module("share/combine_api",function(n,a,i){var e=n("base/tangram").T,s=n("base/class").Class,o=n("share/api_base");a.CombineApi=s.create(function(n){var a=this,i=null,e=null;a.setApi=function(n,a){i=n,e=a},a._init=function(){i&&e&&i.on("sharecompleted",function(n){})}},o.ApiBase)});
 //rebuild by neat 
--- static/api/js/trans/logger.js
+++ static/api/js/trans/logger.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 03 2019 10:41:20 GMT+0800 (GMT+08:00)
+// build time:Wed Apr 03 2019 11:22:40 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
