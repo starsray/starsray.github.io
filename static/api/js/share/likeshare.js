2945593b5d30f10db35a416538efdcbe6225282e
hconly <3344758676@qq.com>
2019-04-17 16:42:13 +0800

    Site updated: 2019-04-17 16:42:11

--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 17 2019 16:35:17 GMT+0800 (GMT+08:00)
+// build time:Wed Apr 17 2019 16:42:10 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
--- static/api/js/share/slide_api.js
+++ static/api/js/share/slide_api.js
@@ -1,3 +1,3 @@
-// build time:Wed Apr 17 2019 16:35:17 GMT+0800 (GMT+08:00)
+// build time:Wed Apr 17 2019 16:42:10 GMT+0800 (GMT+08:00)
 window._bd_share_main.F.module("share/slide_api",function(e,i,t){var n=e("base/tangram").T,a=e("base/class").Class,o=e("share/api_base");i.Api=a.create(function(e){var i=this;i._init=function(){var e=i.getView();e.render(),e.init()},i._slidePop=function(i,t){i._popupBox=t.boxEle,i._getPosition=function(){return{top:0,left:e.bdPos=="left"?0:t.element.width()}},i.show(t,e)},i._processAction=function(e){return{data:{type:"share"}}},i._distory=function(){}},o.ApiBase)});
 //rebuild by neat 
