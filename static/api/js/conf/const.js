ab6f224e8c1e8b8b7f614a1cb35b0293597c84ea
hconly <3344758676@qq.com>
2019-04-28 16:39:00 +0800

    Site updated: 2019-04-28 16:38:58

--- static/api/js/conf/const.js
+++ static/api/js/conf/const.js
@@ -1,3 +1,3 @@
-// build time:Sun Apr 28 2019 16:15:04 GMT+0800 (GMT+08:00)
+// build time:Sun Apr 28 2019 16:38:55 GMT+0800 (GMT+08:00)
 !window._bd_share_is_recently_loaded&&window._bd_share_main.F.module("conf/const",function(t,a,c){a.CMD_ATTR="data-cmd",a.CONFIG_TAG_ATTR="data-tag",a.URLS={likeSetUrl:"http://like.baidu.com/set",commitUrl:"http://s.share.baidu.com/commit",jumpUrl:"http://s.share.baidu.com",mshareUrl:"http://s.share.baidu.com/mshare",emailUrl:"http://s.share.baidu.com/sendmail",nsClick:"http://nsclick.baidu.com/v.gif",backUrl:"http://s.share.baidu.com/back",shortUrl:"http://dwz.cn/v2cut.php"}});
 //rebuild by neat 
--- static/api/js/trans/logger.js
+++ static/api/js/trans/logger.js
@@ -1,3 +1,3 @@
-// build time:Sun Apr 28 2019 16:15:04 GMT+0800 (GMT+08:00)
+// build time:Sun Apr 28 2019 16:38:55 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
